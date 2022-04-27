7d7c26cbc71ef6dc5c7576528d5793d6453645ea
Hector Romero <hromero.mx@gmail.com>
2022-04-27 05:31:43 -0700

    Add tiny font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        tiny: '.75rem',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')

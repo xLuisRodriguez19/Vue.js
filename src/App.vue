d9244edf5af929e1ff6e9eacdfa618f95c15aeda
Hector Romero <hromero.mx@gmail.com>
2022-04-27 05:04:32 -0700

    Estilos basicos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="h-screen container mx-auto">
-    <div class="h-full flex flex-col items-center justify-center">
-      <div class="flex items-center space-x-2 text-5xl">
-        <div>
-          <FaIcon :icon="['fab', 'linux']"/>
-        </div>
-        <div class="font-semibold">Hello Flisol 2022</div>
+  <div class="h-screen flex flex-col">
+    <Header class="flex-shrink-0">
+
+    </Header>
+    <div class="flex-grow container mx-auto max-w-6xl mt-10">
+      <div class="flex items-center justify-center space-x-2 text-5xl font-medium">
+        <FaIcon :icon="['fab', 'linux']"/>
+        <div>Hello Flisol 2022</div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import Header from '@/components/Header.vue'
+import {defineComponent} from "vue";
 
+export default defineComponent({
+  name: 'App',
+  components: { Header }
+})
 </script>
+
+<style>
+#app {
+background: hsl(180, 52%, 96%);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <div class="container max-w-6xl mx-auto">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export  default defineComponent({
+  name: 'Header'
+})
+</script>
+
+<style>
+.header {
+  height: 156px;
+  background: url(/images/bg-header-desktop.svg) hsl(180, 29%, 50%) top center repeat-x;
+}
+</style>
